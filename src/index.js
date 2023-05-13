bda05042c86de44a719ddedf67d2cfc7e0a7d923
rnppnr <rnprice@hotmail.com>
2023-05-13 15:56:00 +0100

    Add instructions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import '../public/style.css';
 const card = document.querySelector('.card');
 const quoteText = document.querySelector('#quote-text');
 const authorText = document.querySelector('#quote-author');
+const infoBtn = document.querySelector('.infoBtn');
+const info = document.querySelector('.info');
 
 const getQuote = async () => {
   fetch('https://api.quotable.io/random?maxLength=125')
@@ -13,6 +15,16 @@ const getQuote = async () => {
     })
 };
 
+infoBtn.addEventListener('click', () => {
+  const displayOpen = infoBtn.data === 'open';
+  displayOpen ?
+    infoBtn.innerHTML = '<span class="open">&#9432;</span>' :
+    infoBtn.innerHTML = '<span class="close">&#x2715;</span>';
+  displayOpen ? card.style.display = 'flex' : card.style.display = 'none';
+  displayOpen ? info.style.display = 'none' : info.style.display = 'flex';
+  displayOpen === true ? infoBtn.data = 'close' : infoBtn.data = 'open';
+});
+  
 card.addEventListener('click', () => {
   getQuote();
 });
@@ -21,3 +33,5 @@ if (quoteText.innerHTML === '') {
   getQuote();
 } 
 
+infoBtn.click();
+infoBtn.click();
